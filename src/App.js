e394a8064797fd36b6852056c44ad02046c697ff
salhindi <calisaraalhindi@gmail.com>
2022-01-31 00:50:02 -0800

    grid and css added"

--- src/App.js
+++ src/App.js
@@ -1,51 +1,45 @@
-// import CardsContainer from './containers/CardsContainer'
 import { useState } from 'react'
+import './App.css'
 
 const cardImages = [
-  {"src": '/img/strawberry.jpg'},
-  {"src": '/img/watermelon.jpg'},
-  {"src": '/img/banana.jpg'},
-  {"src": '/img/peach.jpg'},
-  {"src": '/img/orange.jpg'},
-  {"src": '/img/kiwi.jpg'},
-  {"src": '/img/apple.jpg'},
-  {"src": '/img/grapes.jpg'}
+  {"src": '/fruit_images/strawberry.jpg'},
+  {"src": '/fruit_images/watermelon.jpg'},
+  {"src": '/fruit_images/banana.jpg'},
+  {"src": '/fruit_images/peach.jpg'},
+  {"src": '/fruit_images/orange.jpg'},
+  {"src": '/fruit_images/kiwi.jpg'},
+  {"src": '/fruit_images/apple.jpg'},
+  {"src": '/fruit_images/grapes.jpg'}
 ]
 function App() {
-  const [cards, setCards] = useState([])
+    const [cards, setCards] = useState([])
 
-  
-  //duplicate each card once
-  //randomize order using sort
-  //assign random for id
+    const shuffleCards = () => {
+      const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ... card, id: Math.random() }))
 
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-    .sort(() => Math.random() - 0.5)
-    .map((card) => ({ ... card, id: Math.random() }))
-    setCards(shuffledCards)
-  }
-
-  console.log(cards)
-
+      setCards(shuffledCards)
+    }
 
   return (
-    <div>
-                    <h1> Welcome to Memory game</h1> 
+        <div className="App">
+            <h1> Welcome to Memory game</h1> 
+                 <div>
+                      <button onClick={shuffleCards}>New Game</button>   
+                      <div className="grid">
+                        { cards.map(card => (
+                          <div key={card.id} className='card'>
+                              <div>
+                                <img className="front" src={card.src} alt="front of card" />
+                                <img className="back" src='/pattern.jpg' alt="back of card" />
+                                </div>
 
-  <button onClick={shuffleCards}>New Game</button>    
-        {/* <CardsContainer />
-        <CardsContainer />
-        <CardsContainer />
-        <CardsContainer />
-        <CardsContainer />
-        <CardsContainer />
-        <CardsContainer />
-        <CardsContainer /> */}
-
-
- 
-    </div>
+                            </div>
+                        ))}
+                        </div> 
+                  </div>
+        </div>
   );
 }
 
